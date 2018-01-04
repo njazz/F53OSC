44dde7d94d41747699fca4f2e2ebb462c2e82d32
Alex Nadzharov <anadjarov@gmail.com>
2018-01-04 14:58:57 +0300

    initial commit

---
+++ Framework/F53OSC/F53OSC/F53OSCFramework.h
@@ -0,0 +1,21 @@
+//
+//  F53OSC.h
+//  F53OSC
+//
+//  Created by Alex on 04/01/2018.
+//  Copyright © 2018 Alex Nadzharov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "F53OSC.h"
+
+//! Project version number for F53OSC.
+FOUNDATION_EXPORT double F53OSCVersionNumber;
+
+//! Project version string for F53OSC.
+FOUNDATION_EXPORT const unsigned char F53OSCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <F53OSC/PublicHeader.h>
+
+
---
+++ Framework/F53OSC/F53OSCTests/F53OSCTests.m
@@ -0,0 +1,32 @@
+//
+//  F53OSCTests.m
+//  F53OSCTests
+//
+//  Created by Alex on 04/01/2018.
+//  Copyright © 2018 Alex Nadzharov. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "F53OSC.h"
+
+@interface F53OSCTests : XCTestCase
+
+@end
+
+@implementation F53OSCTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+
+
+
+@end
